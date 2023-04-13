d83dc831ab3ed238fd87942492cd0765cf1dcc3f
rakib3004 <bsse1129@iit.du.ac.bd>
2023-04-13 08:41:59 +0600

    feature/1: add signup form

--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -1,69 +1,86 @@
-export default function SignupForm() {
+import { useState } from "react";
+
+function SignupForm() {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
-  const [agree, setAgree] = useState("");
 
-  const [error, setError] = useState();
-  const [loading, setLoading] = useState();
-
-
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Submitting form:", { username, email, password, confirmPassword });
+    // TODO: Add form submission logic here
+  };
 
   return (
-    <Form style={{ height: "500px" }} onSubmit={handleSubmit}>
-      <TextInput
-        type="text"
-        placeholder="Enter name"
-        icon="person"
-        required
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
 
-      <TextInput
-        type="text"
-        required
-        placeholder="Enter email"
-        icon="alternate_email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
+    <div>
+    <div className="text-center text-2xl font-bold mb-4 text-gray-600">User Sign Up</div>
 
-      <TextInput
-        type="password"
-        required
-        placeholder="Enter password"
-        icon="lock"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
 
-      <TextInput
-        type="password"
-        required
-        placeholder="Confirm password"
-        icon="lock_clock"
-        value={confirmPassword}
-        onChange={(e) => setConfirmPassword(e.target.value)}
-      />
-
-      <Checkbox
-        required
-        text="I agree to the Terms &amp; Conditions"
-        value={agree}
-        onChange={(e) => setAgree(e.target.value)}
-      />
-
-      <Button disabled={loading} type="submit">
-        <span>Submit Now</span>
-      </Button>
-
-      {error && <p className="error">{error}</p>}
-
-      <div className="info">
-        Already have an account? <Link to="/login">Login</Link> instead.
+    <form onSubmit={handleSubmit} className="max-w-sm mx-auto">
+      <div className="mb-4">
+        <label htmlFor="username" className="block text-gray-700 font-bold mb-2">
+          Username
+        </label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          required
+        />
       </div>
-    </Form>
+      <div className="mb-4">
+        <label htmlFor="email" className="block text-gray-700 font-bold mb-2">
+          Email
+        </label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          required
+        />
+      </div>
+      <div className="mb-4">
+        <label htmlFor="password" className="block text-gray-700 font-bold mb-2">
+          Password
+        </label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          required
+        />
+      </div>
+      <div className="mb-4">
+        <label htmlFor="confirm-password" className="block text-gray-700 font-bold mb-2">
+          Confirm Password
+        </label>
+        <input
+          type="password"
+          id="confirm-password"
+          value={confirmPassword}
+          onChange={(event) => setConfirmPassword(event.target.value)}
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          required
+        />
+      </div>
+      <button
+        type="submit"
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+      >
+        Sign up
+      </button>
+    </form>
+
+    </div>
   );
 }
+
+export default SignupForm;
