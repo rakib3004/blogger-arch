f3130dd2aeabfc1b644b4a70355dd042f494c970
rakib3004 <bsse1129@iit.du.ac.bd>
2023-05-18 20:41:45 +0600

    feature/issue-4-testing-with-vitest: add vitest library

--- src/__tests__/pages/Login.test.jsx
+++ src/__tests__/pages/Login.test.jsx
@@ -1,56 +1,9 @@
-import { render, screen, fireEvent } from "@testing-library/react";
-import { loginUser } from "../services/AuthService";
-import { AuthContext } from "../context/AuthContext";
-import Login from "../path/to/Login";
+import { test, expect } from "vitest";
 
-// Mock the loginUser function
-jest.mock("../services/AuthService", () => ({
-  loginUser: jest.fn(),
-}));
+function sum(a, b) {
+  return a + b;
+}
 
-// Mock the useNavigate hook
-jest.mock("react-router-dom", () => ({
-  useNavigate: jest.fn(),
-}));
-
-describe("Login component", () => {
-  beforeEach(() => {
-    render(
-      <AuthContext.Provider value={{ setLoggedStatusInLogin: jest.fn(), username: "" }}>
-        <Login />
-      </AuthContext.Provider>
-    );
-  });
-
-  test("renders login form with username and password fields", () => {
-    expect(screen.getByLabelText("Username")).toBeInTheDocument();
-    expect(screen.getByLabelText("Password")).toBeInTheDocument();
-  });
-
-  test("displays error message when login fails", async () => {
-    const errorMessage = "Invalid credentials";
-    loginUser.mockRejectedValueOnce({ message: errorMessage });
-
-    fireEvent.change(screen.getByLabelText("Username"), { target: { value: "testuser" } });
-    fireEvent.change(screen.getByLabelText("Password"), { target: { value: "testpassword" } });
-    fireEvent.click(screen.getByText("Login"));
-
-    expect(await screen.findByText(errorMessage)).toBeInTheDocument();
-  });
-
-  test("redirects to '/blogs' after successful login", async () => {
-    const navigateMock = jest.fn();
-    const setLoggedStatusInLoginMock = jest.fn();
-    loginUser.mockResolvedValueOnce({ status: 200 });
-
-    jest.spyOn(ReactRouterDom, "useNavigate").mockReturnValue(navigateMock);
-
-    fireEvent.change(screen.getByLabelText("Username"), { target: { value: "testuser" } });
-    fireEvent.change(screen.getByLabelText("Password"), { target: { value: "testpassword" } });
-    fireEvent.click(screen.getByText("Login"));
-
-    expect(loginUser).toHaveBeenCalledWith("testuser", "testpassword");
-    expect(setLoggedStatusInLoginMock).toHaveBeenCalled();
-    expect(navigateMock).toHaveBeenCalledWith("/blogs");
-  });
+test("add 2 numbers", () => {
+  expect(sum(2,3)).toEqual(5);
 });
--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,13 @@
-// import react from '@vitejs/plugin-react';
-// import { defineConfig } from 'vite';
-// // https://vitejs.dev/config/
-// const BASE_URL = process.env.REACT_APP_BASE_URL;
-// export default defineConfig({
-//   server: {
-//     proxy: {
-//       // '/api/v1': 'http://192.168.1.113:3000',
-//       // '/api/v1': 'https://nice-pink-hen-cap.cyclic.app',
-
-//       '/api/v1': 'https://blogger-mint.onrender.com',
-
-//     },
-//   },
-//   plugins: [react()],
-// });
 
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './setupTests.js',
+  }
 });
