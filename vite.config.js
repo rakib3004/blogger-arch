3e3342c90cd93e2896d75003263aae6cb311b165
rakib3004 <bsse1129@iit.du.ac.bd>
2023-05-24 20:57:42 +0600

    feature/issue-5-local-server: set local server address

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,22 @@
-// import react from '@vitejs/plugin-react';
-// import { defineConfig } from 'vite';
-// // https://vitejs.dev/config/
-// const BASE_URL = process.env.REACT_APP_BASE_URL;
-// export default defineConfig({
-//   server: {
-//     proxy: {
-//       // '/api/v1': 'http://192.168.1.113:3000',
-//       // '/api/v1': 'https://nice-pink-hen-cap.cyclic.app',
-
-//       '/api/v1': 'https://blogger-mint.onrender.com',
-
-//     },
-//   },
-//   plugins: [react()],
-// });
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
-// https://vitejs.dev/config/
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api/v1': 'http://192.168.1.113:3000',
+
+      // '/api/v1': 'https://blogger-mint.onrender.com',
+
+    },
+  },
   plugins: [react()],
 });
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// });
